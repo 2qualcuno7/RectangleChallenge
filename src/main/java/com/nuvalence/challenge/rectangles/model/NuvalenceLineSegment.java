b7f3279919078cb272783130e46dd9f193bbeca7
2qualcuno7 <suarezmjuan@gmail.com>
2022-07-02 18:19:40 -0500

    Line intercepts

--- src/main/java/com/nuvalence/challenge/rectangles/model/NuvalenceLineSegment.java
+++ src/main/java/com/nuvalence/challenge/rectangles/model/NuvalenceLineSegment.java
@@ -21,14 +21,35 @@ public class NuvalenceLineSegment {
         Double slope = this.findSlope();
         if(slope.equals(0.0))
             return Optional.of(this.start.y);
-        if(slope.equals(Double.POSITIVE_INFINITY))
+        if(slope.isInfinite())
             return Optional.empty();
 
         return Optional.of(this.start.y - (this.start.x * slope));
     }
 
-    public Optional<Point2D.Double> intersection(NuvalenceLineSegment otherLine){
-        return null;
+    public Optional<NuvalencePoint> intersection(NuvalenceLineSegment otherLine){
+        Double slopeOne = this.findSlope();
+        Double slopeTwo = otherLine.findSlope();
+        Optional<Double> yInterceptOne = this.yIntercept();
+        Optional<Double> yInterceptTwo = otherLine.yIntercept();
+
+        if (slopeOne.equals(slopeTwo))
+            return Optional.empty();
+
+        if(yInterceptOne.isEmpty())
+            return Optional.of(new NuvalencePoint(this.start.x, otherLine.findSlope() * this.start.x+otherLine.yIntercept().get()));
+
+        if(yInterceptTwo.isEmpty())
+            return Optional.of(new NuvalencePoint(otherLine.start.x, this.findSlope() * otherLine.start.x+this.yIntercept().get()));
+
+        Double x = (yInterceptTwo.get() - yInterceptOne.get()) / (slopeOne - slopeTwo);
+        Double y = slopeOne * x + yInterceptOne.get();
+
+        return Optional.of(new NuvalencePoint(x,y));
+    }
+
+    public Boolean hasSameFormula(NuvalenceLineSegment otherLine){
+        return this.findSlope().equals(otherLine.findSlope()) && this.yIntercept().equals(otherLine.yIntercept());
     }
 
     public boolean isSubsegment(NuvalenceLineSegment otherLine){
--- src/test/java/com/nuvalence/challenge/rectangles/model/NuvalenceLineSegmentTest.java
+++ src/test/java/com/nuvalence/challenge/rectangles/model/NuvalenceLineSegmentTest.java
@@ -105,4 +105,144 @@ public class NuvalenceLineSegmentTest {
 
         assertEquals(optionalRealValue.get(), 2.0, 0.0);
     }
+
+    //Interceptions
+    @Test
+    void randomSlopes(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(3.0, 4.2),
+                new NuvalencePoint(4.5, -1.6)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(-7.1, 9.0),
+                new NuvalencePoint(0.6, 0.0)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+        Double expectedValueX = 5.59659820282;
+        Double expectedValueY = -5.84017971759;
+
+        assertEquals(expectedValueX, optionalRealValue.get().getX(), 0.000001);
+        assertEquals(expectedValueY, optionalRealValue.get().getY(), 0.000001);
+    }
+
+    @Test
+    void sameLine(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(1.0, 0.4),
+                new NuvalencePoint(6.0, 3.9)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(-2.0, -1.7),
+                new NuvalencePoint(0.0, -0.3)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        assertEquals(optionalRealValue, Optional.empty());
+    }
+
+    @Test
+    void parallelLines(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(0.0, 4.0),
+                new NuvalencePoint(-3.0, -2.0)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(1.0, 1.0),
+                new NuvalencePoint(-5.0, -11.0)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        assertEquals(optionalRealValue, Optional.empty());
+    }
+
+    @Test
+    void oneVerticalTwoRandom(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(2.5, 3.1),
+                new NuvalencePoint(2.5, -1.9)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(3.2, 0.0),
+                new NuvalencePoint(1.5, 9.8)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        Double expectedValueX = 2.5;
+        Double expectedValueY = 4.03529411765;
+
+        assertEquals(expectedValueX, optionalRealValue.get().getX(), 0.000001);
+        assertEquals(expectedValueY, optionalRealValue.get().getY(), 0.000001);
+    }
+
+    @Test
+    void oneRandomTwoVertical(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(-1.3, 4.1),
+                new NuvalencePoint(1.8, -3.0)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(1.7, 9.0),
+                new NuvalencePoint(1.7, -7.0)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        Double expectedValueX = 1.7;
+        Double expectedValueY = -2.77096774194;
+
+        assertEquals(expectedValueX, optionalRealValue.get().getX(), 0.000001);
+        assertEquals(expectedValueY, optionalRealValue.get().getY(), 0.000001);
+    }
+
+    @Test
+    void oneHorizontalTwoRandom(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(3.9, 0.0),
+                new NuvalencePoint(0.2, 0.0)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(7.9, 4.1),
+                new NuvalencePoint(-0.8, 3.4)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        Double expectedValueX = -43.0571428571;
+        Double expectedValueY = 0.0;
+
+        assertEquals(expectedValueX, optionalRealValue.get().getX(), 0.000001);
+        assertEquals(expectedValueY, optionalRealValue.get().getY(), 0.000001);
+    }
+
+    @Test
+    void oneRandomTwoHorizontal(){
+        NuvalenceLineSegment lineOne = new NuvalenceLineSegment(
+                new NuvalencePoint(9.8, 0.2),
+                new NuvalencePoint(1.4, 5.3)
+        );
+
+        NuvalenceLineSegment lineTwo = new NuvalenceLineSegment(
+                new NuvalencePoint(8.0, 3.6),
+                new NuvalencePoint(-1.7, 3.6)
+        );
+
+        Optional<NuvalencePoint> optionalRealValue = lineOne.intersection(lineTwo);
+
+        Double expectedValueX = 4.2;
+        Double expectedValueY = 3.6;
+
+        assertEquals(expectedValueX, optionalRealValue.get().getX(), 0.000001);
+        assertEquals(expectedValueY, optionalRealValue.get().getY(), 0.000001);
+    }
+
 }
